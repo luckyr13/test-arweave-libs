dd94fa3a3b326acba3242a34e814fcc8eaf27886
luckyr13 <richie.isc@gmail.com>
2023-04-20 14:32:23 -0600

    Load project files

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+import Arweave from 'arweave'
+import { Warp, WarpFactory } from 'warp-contracts'
+import { onMounted, ref } from 'vue'
+
+const arweave = Arweave.init({})
+const warp = WarpFactory.forMainnet({arweave})
+const cached = ref({})
+onMounted(async () => {
+  const contract = await warp.contract('kYoC3ShwsS8XSCyxM1_sLiMGmm-xrDBvxYEcx1-Dh1s');
+
+  const { sortKey, cachedValue } = await contract.readState();
+  cached.value = cachedValue
+
+})
+
+
+
+
+
+</script>
+
+<template>
+  Hello
+  {{ cached }}
+  
+</template>
